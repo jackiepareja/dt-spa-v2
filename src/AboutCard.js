2f1de18a55575f760039b5f83e8f41cc63a2a28f
jackiepareja <pareja.jackie@gmail.com>
2019-01-17 03:37:22 -0800

    adding state systems with class components.

--- src/AboutCard.js
+++ src/AboutCard.js
@@ -1,21 +1,46 @@
 import React from "react";
 
+const services = [
+  {
+    icon: "./img/dog-brown-tan.svg",
+    title: "Dog Walking"
+  },
+  {
+    icon: "./img/dog-brwn-white.svg",
+    title: "Dog Boarding"
+  },
+  {
+    icon: "./img/dog-grey-white.svg",
+    title: "Dog Sitting"
+  },
+  {
+    icon: "./img/dog-red-brown.svg",
+    title: "Pet Check-in"
+  }
+];
+
 class AboutCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { icon: "", title: "" };
+
+    this.setState({ icon: services.icon });
+    this.setState({ title: services.title });
+  }
+
   render() {
     return (
       <div className="AboutCard">
         <div className="col-sm-6 col-md-3">
           <div className="thumbnail">
-            <img src="https://via.placeholder.com/171x180" />
+            <img src={this.state.icon} />
             <div className="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
+              <h4>{this.state.title}</h4>
+
               <p>
-                <a href="#" class="btn btn-primary" role="button">
-                  Button
-                </a>{" "}
-                <a href="#" class="btn btn-default" role="button">
-                  Button
+                <a href="#" class="btn btn-warning" role="button">
+                  Learn More
                 </a>
               </p>
             </div>
