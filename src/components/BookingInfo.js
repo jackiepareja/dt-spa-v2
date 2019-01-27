ec5aac21517246263b37ae2387f24e0d56a174a2
jackiepareja <pareja.jackie@gmail.com>
2019-01-27 12:30:52 -0800

    spa contactform

--- src/components/BookingInfo.js
+++ src/components/BookingInfo.js
@@ -18,8 +18,12 @@ class BookingInfo extends React.Component {
                   id="name"
                   required
                 />
-                <label for="name" className="form__label" />
+                <label for="name" className="form__label">
+                  Full name
+                </label>
+              </div>
 
+              <div className="form__group">
                 <input
                   type="email"
                   className="form__input"
@@ -27,7 +31,44 @@ class BookingInfo extends React.Component {
                   id="email"
                   required
                 />
-                <label for="email" className="form__label" />
+                <label for="email" className="form__label">
+                  Email address
+                </label>
+              </div>
+
+              <div className="form__group">
+                <input
+                  type="radio"
+                  id="walk"
+                  className="form__radio-group"
+                  name="service"
+                />
+                <label for="walk" className="form__radio-label">
+                  <span className="form__radio-button" />
+                  Dog Walking
+                </label>
+
+                <input
+                  type="radio"
+                  id="boarding"
+                  className="form__radio-group"
+                  name="service"
+                />
+                <label for="boarding" className="form__radio-label">
+                  <span className="form__radio-button" />
+                  Dog Boarding
+                </label>
+
+                <input
+                  type="radio"
+                  id="checkin"
+                  className="form__radio-group"
+                  name="service"
+                />
+                <label for="checkin" className="form__radio-label">
+                  <span className="form__radio-button" />
+                  Pet Check-in
+                </label>
               </div>
             </form>
           </div>
