7ddfcdc42dff8c992c3f0d0788f89124e29c0bb2
jackiepareja <pareja.jackie@gmail.com>
2019-02-09 19:43:15 -0800

    added about content

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -9,20 +9,20 @@ class AboutText extends React.Component {
             Dog walks like you never have before
           </h3>
           <p className="paragraph">
-            Lorem ipsum dolor amet schlitz cornhole air plant, 90's affogato
-            glossier small batch chillwave. Pok pok trust fund tbh bushwick
-            schlitz whatever PBR&B. Deep v pinterest vegan chia, blue bottle
-            palo santo vape unicorn kinfolk raclette tacos small batch pug.
+            Life gets busy, so we aim to make sure that your doggie's schedule
+            is packed with fun walks and adventures. From private and relaxed
+            walks, to adventurous social group walks, you can live through your
+            dog's walks vivaciously!
           </p>
 
-          <h3 className="heading-tertiary u-margin-bottom-small">
-            Live through your dog's walks vivaciously
-          </h3>
+          <h3 className="heading-tertiary u-margin-bottom-small">Our Story</h3>
           <p className="paragraph">
-            Kickstarter marfa helvetica intelligentsia wayfarers disrupt.
-            Freegan hexagon raw denim portland listicle, hell of selvage
-            farm-to-table vice letterpress small batch organic kogi tilde
-            pickled.
+            We're a pet services company based in San Francisco and we launched
+            in 2017. In just a less than a year, we've grown to almost 100
+            doggie clients and 6 tight-knit team members. Our clients have
+            regarded us as consistent, safe and trust-worthy. We put a lot of
+            time and effort in our walk routes to ensure our walks are enjoyable
+            and within the routine.
           </p>
 
           <a href="/" className="btn-text">
