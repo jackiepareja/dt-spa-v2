27f1b31c5db57fc7167757075d4b7be021ba017d
jackiepareja <jaxpaer@gmail.com>
2019-07-10 22:43:47 -0700

    added console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { Component} from "react";
 import ReactDOM from "react-dom";
 
-import "./sass/index.scss";
-//import "./res/styles/css/style.css";
+// import "./sass/index.scss";
+// import "./res/styles/css/style.css";
 import "./res/styles/icon-font.css";
 
 import Home from "./pages/Home";
+
+console.log("%cReact Exists @JackieP", "color: yellow; font-size: 20px");
 
 class App extends Component {
   render() {
